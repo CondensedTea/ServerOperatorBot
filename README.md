d8908aad701ac90c9d9c751be8a80270382785e4
greentea <tysh3@yandex.ru>
2020-10-30 16:16:00 +0300

    Logginig /w emoji, user filter fix, get_name func

---
+++ README.md
@@ -0,0 +1,11 @@
+## Server Operator Bot
+
+This bot allows to operate homebrew VDI based on Hetzner Cloud servers.
+
+It is based on `hcloud-python` and `python-telegram-bot` libs.
+
+Data is stored in json files which can be specified in #config section.
+
+Bot replies stored in Text class and can be modified. 
+
+As for now bot must be ran on Samba DC in order to correctly cleanup machines from AD.
--- ServerOperator.py
+++ ServerOperator.py
@@ -25,11 +25,14 @@ admin_filter.add_user_ids(admins_list)
 
 
 def start(update, context):
+    user_list = []
+    user_filter = Filters.user()
+    user_filter.add_user_ids(user_list)
     data = load_json(data_file)
     invites = load_json(invites_file)
     join_token = extract_join_token(context.args)
     user_id = update.message.from_user.id
-    name = f'{update.message.chat.first_name}{"" if update.message.chat.last_name is None else " "+update.message.chat.last_name}'
+    name = get_name(update)
     if join_token in invites.keys():
         if f'{user_id}' not in data.keys():
             if invites[f'{join_token}'] == "":
@@ -38,23 +41,32 @@ def start(update, context):
                 flush_json(data_file, data)
                 flush_json(invites_file, invites)
                 context.bot.send_message(chat_id=update.effective_chat.id, text=Text.welcome)
+                logging.info(f'✅ {name}({user_id}) was successfully registered')
             else:
                 context.bot.send_message(chat_id=update.effective_chat.id, text=Text.broken_link)
+                logging.warning(f'⚠️ {name}({user_id}) tried to use taken link')
         else:
             context.bot.send_message(chat_id=update.effective_chat.id, text=Text.user_in_base)
+            logging.warning(f'🤔 {name}({user_id}) tried to register again')
+    else:
+        logging.warning(f'❌ {name}({user_id}) tried to register without real link')
 
 
 def gen_link(update, context):
+    user_id = update.message.from_user.id
+    name = get_name(update)
     invites = load_json(invites_file)
     current_join_token = gen_join_token()
     invites[f'{current_join_token}'] = ""
     flush_json(invites_file, invites)
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'https://t.me/ServerOperatorBot?start={current_join_token}')
+    logging.info(f'🔗 Invite link was made by {name}({user_id})')
 
 
 def open_server(update, context):
     user_id = update.message.from_user.id
     data = load_json(data_file)
+    name = get_name(update)
     try:
         if data[f'{user_id}']["server_ip"] == "":
             ip = get_ip_address(data, user_id)
@@ -70,17 +82,21 @@ def open_server(update, context):
             data[f'{user_id}']["server_id"] = create_response.server.id
             flush_json(data_file, data)
             context.bot.send_message(chat_id=update.effective_chat.id, text=t.creation_complete())
+            logging.info(f'⬆️ {name}({user_id}) created server Cloud-PC-{ip}')
         else:
             ip = data[f'{user_id}']["server_ip"]
             t = Text(ip)
             context.bot.send_message(chat_id=update.effective_chat.id, text=t.user_have_server())
+            logging.warning(f'⚠️ {name}({user_id}) tried create second server')
             return
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text=t.open_server_error)
+        logging.error(f'❌ {name}({user_id}) could not create server')
 
 
 def close_server(update, context):
     user_id = update.message.from_user.id
+    name = get_name(update)
     data = load_json(data_file)
     ip = data[f'{user_id}']["server_ip"]
     t = Text(ip)
@@ -94,8 +110,10 @@ def close_server(update, context):
         data[f'{user_id}']["server_id"] = ""
         flush_json(data_file, data)
         context.bot.send_message(chat_id=update.effective_chat.id, text=t.deletion_complete())
+        logging.info(f'⬇️ {name}({user_id}) deleted server Cloud-PC-{ip}')
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text=t.deletion_error())
+        logging.error(f'❌ {name}({user_id}) could not delete server Cloud-PC-{ip}')
 
 
 def gen_join_token():
@@ -128,14 +146,14 @@ def flush_json(json_file, data):
         json.dump(data, file, indent=4)
 
 
+def get_name(update):
+    return f'{update.message.chat.first_name}{"" if update.message.chat.last_name is None else " "+update.message.chat.last_name}'
+
+
 def main():
     user_list = []
-    data = load_json(data_file)
-    for user in data.keys():
-        user_list += user
     user_filter = Filters.user()
     user_filter.add_user_ids(user_list)
-
     updater = Updater(token=os.environ["TOKEN_SO"], use_context=True)
 
     dp = updater.dispatcher
@@ -148,3 +166,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    logging.info("✅ Bot was started  ")
