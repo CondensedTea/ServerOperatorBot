3c9761716dd697b3fcce63d0e8669ad0128672d4
greentea <tysh3@yandex.ru>
2021-01-14 12:49:35 +0300

    more fixes to request_server.py

--- request_server.py
+++ request_server.py
@@ -8,12 +8,13 @@ import logging
 
 app = Flask(__name__)
 bot = telegram.Bot(token=os.environ["TOKEN_SO"])
-log_file = os.environ["LOG_FILE"]
+log_file = os.environ["LOGFILE"]
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.WARNING)
 
 logging.basicConfig(filename=log_file, filemode="a", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 sqlite_connector.config(filename=os.environ["SQLITE_DB"], lowest_ip=7)
+
 
 @app.route('/get_name')
 def get_name():
@@ -32,7 +33,8 @@ def ready_up(name):
     if not u:
         logging.warning("Received call for /ready_up from unknown ip -- {}".format(request.remote_addr))
     else:
-        bot.sendMessage(chat_id=u.ready_up(), text="Удаленный рабочий стол готов к работе")
+        (user_id,) = u.ready_up()
+        bot.sendMessage(chat_id=user_id, text="Удаленный рабочий стол готов к работе")
         logging.info("/ready_up was successfully called from {}".format(request.remote_addr))
         return '', 202
 
